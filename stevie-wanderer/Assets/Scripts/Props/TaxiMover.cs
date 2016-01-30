bba5c9f9fba0c6262c9d89ac756e2ee9ac85cd89
jehna <jesse.luoto@gmail.com>
2016-01-31 00:21:47 +0200

    Added: Speeding Taxi

--- stevie-wanderer/Assets/Scripts/Props/TaxiMover.cs
+++ stevie-wanderer/Assets/Scripts/Props/TaxiMover.cs
@@ -4,9 +4,25 @@ using System.Collections;
 public class TaxiMover : StevieKiller {
 
 	public float speed = 0.1f;
+	public float spawnDistance = 50.0f;
+
+	void Start() {
+		base.Start ();
+		InvokeRepeating ("SpawnNewTaxi", 10.0f, 10.0f);
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.position += Vector3.forward * speed;
 	}
+
+	void SpawnNewTaxi() {
+		this.speed += 0.04f;
+		if (this.transform.position.z > GetStevie ().position.z + spawnDistance) {
+			Vector3 newPos = this.transform.position;
+			newPos.z = GetStevie ().position.z - spawnDistance;
+			this.transform.position = newPos;
+		}
+	}
+
 }
