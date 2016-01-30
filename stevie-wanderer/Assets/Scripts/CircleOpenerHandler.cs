fca5712889c7e44c5523ae46d0491682d99f5609
Jarkka <jarkka.salminen@createtrips.com>
2016-01-31 00:15:26 +0200

    Fixed: Ugliness bugs with Circle opener

--- stevie-wanderer/Assets/Scripts/CircleOpenerHandler.cs
+++ stevie-wanderer/Assets/Scripts/CircleOpenerHandler.cs
@@ -5,6 +5,9 @@ public class CircleOpenerHandler : MonoBehaviour {
 
 	public GameObject circleOpener;
 
+	private float maxScaleValue = 2000.0f;
+	private float minScaleValue = 0.85f;
+
 	public void CloseCircle () {
 		StartCoroutine("CloseCircleCoroutine");
 	}
@@ -13,13 +16,13 @@ public class CircleOpenerHandler : MonoBehaviour {
 
 		this.circleOpener.SetActive(true);
 
-		float elapsedTime = 0;
+		float elapsedTime = 0.0f;
 		float time = 1.0f;
 
-		while (elapsedTime < time) {
-			float newLocalScale = Mathf.Lerp(2000, 0.85f, (elapsedTime / time));
+		while (this.circleOpener.transform.localScale.x > this.minScaleValue) {
+			float newLocalScale = Mathf.Lerp(this.maxScaleValue, this.minScaleValue, (elapsedTime / time));
 			elapsedTime += Time.deltaTime;
-
+			Debug.Log(newLocalScale);
 			this.circleOpener.transform.localScale = new Vector3(newLocalScale,newLocalScale,newLocalScale);
 
 			yield return new WaitForEndOfFrame();
@@ -34,11 +37,11 @@ public class CircleOpenerHandler : MonoBehaviour {
 
 		this.circleOpener.SetActive(true);
 
-		float elapsedTime = 0;
-		float time = 2.0f;
+		float elapsedTime = 0.0f;
+		float time = 2.5f;
 
-		while (elapsedTime < time) {
-			float newLocalScale = Mathf.Lerp(0.85f, 2000, (elapsedTime / time));
+		while (this.circleOpener.transform.localScale.x < this.maxScaleValue) {
+			float newLocalScale = Mathf.Lerp(this.minScaleValue, this.maxScaleValue, (elapsedTime / time));
 			elapsedTime += Time.deltaTime;
 
 			this.circleOpener.transform.localScale = new Vector3(newLocalScale,newLocalScale,newLocalScale);
--- stevie-wanderer/Assets/Scripts/MenuHandler.cs
+++ stevie-wanderer/Assets/Scripts/MenuHandler.cs
@@ -28,6 +28,8 @@ public class MenuHandler : MonoBehaviour {
 		if (this.startScreen.activeSelf && Input.anyKeyDown) {
 			this.startScreen.SetActive(false);
 			this.menuScreen.SetActive(true);
+		} else if (this.menuScreen.activeSelf && Input.GetKeyDown(KeyCode.Return) && !this.circleOpenerHandler.circleOpener.activeSelf) {
+			StartGameButtonPressed();
 		}
 	}
 
@@ -39,6 +41,7 @@ public class MenuHandler : MonoBehaviour {
 
 	public void StartGameButtonPressed () {
 		if (this.gameStarted == false) {
+			this.gameStarted = true;
 			StartCoroutine("StartGameWithDelay");
 		}
 	}
@@ -53,7 +56,7 @@ public class MenuHandler : MonoBehaviour {
 
 		this.circleOpenerHandler.CloseCircle();
 
-		yield return new WaitForSeconds(3.0f);
+		yield return new WaitForSeconds(1.5f);
 
 		SceneManager.LoadScene(1);
 	}
