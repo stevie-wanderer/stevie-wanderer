4d2e92f6ab16d2b0859ca4d2e377fdbae2205edd
jehna <jesse.luoto@gmail.com>
2016-01-30 12:39:00 +0200

    Added: Stevie's speed to be bound to actual velocity

--- stevie-wanderer/Assets/Scripts/Actors (uuko ja koira)/Stevie.cs
+++ stevie-wanderer/Assets/Scripts/Actors (uuko ja koira)/Stevie.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Stevie : MonoBehaviour {
 
+	public Animator stevieAnimator;
+
 	void Start() {
 		this.GetComponent<Rigidbody> ().velocity = Vector3.forward;
 	}
@@ -10,5 +12,11 @@ public class Stevie : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		this.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 10);
+	}
+
+	void Update() {
+		// Look at the direction where you're going
+		transform.LookAt (this.transform.position + this.GetComponent<Rigidbody> ().velocity);
+		stevieAnimator.speed = this.GetComponent<Rigidbody> ().velocity.sqrMagnitude / 1.4f;
 	}
 }
