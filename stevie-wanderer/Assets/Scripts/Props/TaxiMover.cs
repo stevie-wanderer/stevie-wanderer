86db99534b233acc14519eedef0d61d30a876a86
jehna <jesse.luoto@gmail.com>
2016-01-30 17:22:38 +0200

    Added: Taxi materials and prefab

---
+++ stevie-wanderer/Assets/Scripts/Props/TaxiMover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaxiMover : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += Vector3.forward * speed;
+	}
+}
---
+++ stevie-wanderer/Assets/Scripts/Props/TaxiWheelRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaxiWheelRotator : MonoBehaviour {
+
+	public Vector3 rotateAround = Vector3.left;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotateAround);
+	}
+}
