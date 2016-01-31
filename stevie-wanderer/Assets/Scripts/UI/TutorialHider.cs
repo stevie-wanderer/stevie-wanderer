7e430372dbfc1aa3e47c3286b3a081372a70ba09
jehna <jesse.luoto@gmail.com>
2016-01-31 14:26:05 +0200

    Added: Tutorial hider and Stevie meter meter

---
+++ stevie-wanderer/Assets/Scripts/UI/TutorialHider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialHider : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0) {
+			this.gameObject.SetActive (false);
+		}
+	}
+}
